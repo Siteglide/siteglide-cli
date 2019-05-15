00836516a74fc3587e9455322d1c772db17106b3
Dean Vizer <dean@siteglide.com>
2019-05-15 12:25:41 +0100

    Remove unused tests

--- test/lib/commands.test.js
+++ test/lib/commands.test.js
@@ -3,51 +3,24 @@ var silentState = sh.config.silent; // save old silent state
 
 test('should return error for missing command on stdout', ()  => {
   sh.config.silent = true;
-  let command = sh.exec('./siteglide.js missing');
+  let command = sh.exec('./siteglide-cli.js missing');
   expect(command.stderr).toEqual(expect.stringContaining('unknown command: missing'));
   sh.config.silent = silentState;
 });
 
-
-// test('should run env list', ()  => {
-//   sh.config.silent = true;
-//   let command = sh.exec('./siteglide.js env list');
-//   expect(command.code).toEqual(1);
-//   expect(command.stderr).toEqual(expect.stringContaining('No environments registered yet, please see siteglide-cli env add'));
-//   sh.config.silent = silentState;
-// });
-
 test('should run help on env add', ()  => {
   sh.config.silent = true;
-  let command = sh.exec('./siteglide.js env add');
+  let command = sh.exec('./siteglide-cli-add.js env add');
   expect(command.code).toEqual(0);
-  expect(command.stdout).toEqual(expect.stringContaining('Usage: siteglide [options] [command]'));
+  expect(command.stdout).toEqual(expect.stringContaining('Usage: siteglide-cli add [options] [command]'));
   sh.config.silent = silentState;
 });
-
-// test('should run help on gui serve', ()  => {
-//   sh.config.silent = true;
-//   let command = sh.exec('./siteglide.js gui serve');
-//   expect(command.code).toEqual(0);
-//   expect(command.stdout).toEqual(expect.stringContaining('Usage: siteglide-cli-gui-serve [options] [environment]'));
-//   sh.config.silent = silentState;
-// });
-
-// test('should run help on logs', ()  => {
-//   sh.config.silent = true;
-
-//   let command = sh.exec('./siteglide.js logs');
-//   expect(command.code).toEqual(0);
-//   expect(command.stdout).toEqual(expect.stringContaining('Usage: siteglide-cli-logs [options] [environment]'));
-//   sh.config.silent = silentState;
-// });
-
 
 test('should run help on sync', ()  => {
   sh.config.silent = true;
 
-  let command = sh.exec('./siteglide.js sync');
+  let command = sh.exec('./siteglide-cli.js sync');
   expect(command.code).toEqual(0);
-  expect(command.stdout).toEqual(expect.stringContaining('Usage: siteglide-sync [options] [environment]'));
+  expect(command.stdout).toEqual(expect.stringContaining('Usage: siteglide-cli sync [environment] [options]'));
   sh.config.silent = silentState;
 });
