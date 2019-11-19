27ceca8d3b59ba3024ea867758ffe1af1043c9bd
Dean Vizer <dean@siteglide.com>
2019-11-19 15:19:28 +0000

    Temporarily remove export

--- siteglide-cli.js
+++ siteglide-cli.js
@@ -19,10 +19,10 @@ program
 	.command('sync [environment]', 'update site on local file change')
 	.command('pull [environment]', 'get all files from site')
 	.command('graphql [environment]', 'gui for graphql')
-	.command('logs [environment]', 'stream debugging logs from your site')
+	.command('logs [environment]', 'stream debugging logs from your website')
 	.command('init', 'create default folder structure for Siteglide Admin')
 	.command('deploy', 'upload all code to your site')
-	.command('export', 'export the code, assets and data from your site')
+	// .command('export', 'export the code, assets and data from your site')
 	.parse(process.argv);
 
 if (program._execs instanceof Set){
