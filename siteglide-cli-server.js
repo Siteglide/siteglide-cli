bf9db2fc8494f58d685e9b4841c3abafc653ed27
Dean Vizer <dean@siteglide.com>
2020-03-25 12:37:32 +0000

    GraphQL - Add automatic browser open option

--- siteglide-cli-server.js
+++ siteglide-cli-server.js
@@ -6,44 +6,49 @@ const express = require('express'),
 	Gateway = require('./lib/proxy'),
 	logger = require('./lib/logger');
 
-const port = process.env.PORT || 3333;
-const app = express();
+const start = (env) => {
+	const port = env.PORT || 3333;
+	const app = express();
 
-const gateway = new Gateway({
-	url: process.env.SITEGLIDE_URL,
-	token: process.env.SITEGLIDE_TOKEN,
-	email: process.env.SITEGLIDE_EMAIL
-});
+	const gateway = new Gateway({
+		url: env.SITEGLIDE_URL,
+		token: env.SITEGLIDE_TOKEN,
+		email: env.SITEGLIDE_EMAIL
+	});
 
-app.use(bodyParser.json());
-app.use(compression());
-app.use('/gui/graphql', express.static(__dirname + '/gui/graphql/public'));
+	app.use(bodyParser.json());
+	app.use(compression());
+	app.use('/gui/graphql', express.static(__dirname + '/gui/graphql/public'));
 
-// INFO
-const info = (req, res) => {
-	return res.send(JSON.stringify({ SG_URL: process.env.SITEGLIDE_URL }));
+	// INFO
+	const info = (req, res) => {
+		return res.send(JSON.stringify({ SG_URL: env.SITEGLIDE_URL }));
+	};
+
+	app.get('/info', info);
+
+	// GRAPHQL
+	const graphqlRouting = (req, res) => {
+		gateway
+			.graph(req.body)
+			.then(body => res.send(body))
+			.catch(error => res.send(error));
+	};
+
+	app.post('/graphql', graphqlRouting);
+	app.post('/api/graph', graphqlRouting);
+
+	app.listen(port, err => {
+		if (err) {
+			logger.Error(`Something wrong happened when trying to run Express server: ${err}`);
+		}
+
+		logger.Debug(`Server is listening on ${port}`);
+		logger.Success(`Connected to ${env.SITEGLIDE_URL}`);
+		logger.Success(`GraphQL Browser: http://localhost:${port}/gui/graphql`);
+	});
 };
 
-app.get('/info', info);
-
-
-// GRAPHQL
-const graphqlRouting = (req, res) => {
-	gateway
-		.graph(req.body)
-		.then(body => res.send(body))
-		.catch(error => res.send(error));
+module.exports = {
+	start: start
 };
-
-app.post('/graphql', graphqlRouting);
-app.post('/api/graph', graphqlRouting);
-
-app.listen(port, err => {
-	if (err) {
-		logger.Error(`Something wrong happened when trying to run Express server: ${err}`);
-	}
-
-	logger.Debug(`Server is listening on ${port}`);
-	logger.Success(`Connected to ${process.env.SITEGLIDE_URL}`);
-	logger.Success(`GraphQL Browser: http://localhost:${port}/gui/graphql`);
-});
