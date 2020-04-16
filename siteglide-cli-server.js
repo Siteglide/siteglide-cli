c89d1c7760dd4b79d80fd44f5672540f56279ed9
Dean Vizer <dean@siteglide.com>
2020-04-16 10:59:42 +0100

    GraphiQL - Upgrade version and include explorer

--- siteglide-cli-server.js
+++ siteglide-cli-server.js
@@ -4,7 +4,8 @@ const express = require('express'),
 	compression = require('compression'),
 	bodyParser = require('body-parser'),
 	Gateway = require('./lib/proxy'),
-	logger = require('./lib/logger');
+	logger = require('./lib/logger'),
+	path = require('path');
 
 const start = (env) => {
 	const port = env.PORT || 3333;
@@ -16,9 +17,16 @@ const start = (env) => {
 		email: env.SITEGLIDE_EMAIL
 	});
 
+	const graphqlRouting = (req, res) => {
+		gateway
+			.graph(req.body)
+			.then(body => res.send(body))
+			.catch(error => res.send(error));
+	};
 	app.use(bodyParser.json());
 	app.use(compression());
-	app.use('/gui/graphql', express.static(__dirname + '/gui/graphql/public'));
+
+	app.use('/gui/graphql', express.static(path.resolve(__dirname, 'gui', 'graphql', 'public')));
 
 	// INFO
 	const info = (req, res) => {
@@ -26,26 +34,23 @@ const start = (env) => {
 	};
 
 	app.get('/info', info);
-
-	// GRAPHQL
-	const graphqlRouting = (req, res) => {
-		gateway
-			.graph(req.body)
-			.then(body => res.send(body))
-			.catch(error => res.send(error));
-	};
-
 	app.post('/graphql', graphqlRouting);
 	app.post('/api/graph', graphqlRouting);
 
-	app.listen(port, err => {
-		if (err) {
-			logger.Error(`Something wrong happened when trying to run Express server: ${err}`);
-		}
-
+	app.listen(port, function() {
 		logger.Debug(`Server is listening on ${port}`);
 		logger.Success(`Connected to ${env.SITEGLIDE_URL}`);
 		logger.Success(`GraphQL Browser: http://localhost:${port}/gui/graphql`);
+	})
+	.on('error', err => {
+		if (err.errno === 'EADDRINUSE') {
+			logger.Error(`Port ${port} is already in use.`, { exit: false });
+			logger.Print('\n');
+			logger.Warn('Please use -p <port> to run server on a different port.\n');
+			logger.Warn('Example: pos-cli gui serve staging -p 31337');
+		} else {
+			logger.Error(`Something wrong happened when trying to run Express server: ${err}`);
+		}
 	});
 };
 
