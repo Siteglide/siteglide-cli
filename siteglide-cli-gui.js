442517010adb0c3604c9cf3e2b462a416e3e16cf
Dean Vizer <dean@siteglide.com>
2021-03-29 13:24:52 +0100

    GUI - Fix missing open option

--- siteglide-cli-gui.js
+++ siteglide-cli-gui.js
@@ -3,6 +3,7 @@
 const program = require('commander'),
 	fetchAuthData = require('./lib/settings').fetchSettings,
 	logger = require('./lib/logger'),
+	open = require('open'),
 	server = require('./siteglide-cli-server'),
 	version = require('./package.json').version;
 
@@ -14,6 +15,7 @@ program
 	.arguments('[environment]', 'name of environment. Example: staging')
 	.option('-c --config-file <config-file>', 'config file path', '.siteglide-config')
 	.option('-p --port <port>', 'port number', '3333')
+	.option('-o, --open', 'automatically opens your default browser')
 	.action(async (environment, params) => {
 		process.env.CONFIG_FILE_PATH = params.configFile;
 		const authData = fetchAuthData(environment, program);
@@ -27,6 +29,11 @@ program
 
 		try {
 			await server.start(process.env, 'gui');
+			if(params.open){
+				setTimeout(async function(){
+					await open(`http://localhost:${params.port}/gui/graphql`);
+				},1000);
+			}
 		} catch (e) {
 			logger.Error('GUI failed. Please check that you have the correct permissions or that your site is not locked.');
 		}
