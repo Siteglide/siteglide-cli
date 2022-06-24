59d48447370d0a172764d64755c47f9e87a43869
Dean Vizer <dean@siteglide.com>
2022-06-24 14:22:40 +0100

    Remove semver check

--- scripts/check-node-version.js
+++ scripts/check-node-version.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-const semver = require('semver');
-const engines = require('../package.json').engines;
 const logger = require('../lib/logger');
-const version = engines.node;
 
-if (!semver.satisfies(process.version, version)) {
-	logger.Error(`Required node version ${version} not satisfied with current version ${process.version}.`);
-}else{
-	logger.Success('Successfully installed! Please see https://developers.siteglide.com/introducing-siteglide-cli for more information on Siteglide CLI');
-}
+logger.Success('Successfully installed! Please see https://developers.siteglide.com/introducing-siteglide-cli for more information on Siteglide CLI');
