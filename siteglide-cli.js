609fc3cc85e308b23b08b3a34c56359668aa7748
MattJonesSiteglide <matt.jones@wysi.co.uk>
2026-08-06 10:13:45 +0100

    pos-cli check command added

--- siteglide-cli.js
+++ siteglide-cli.js
@@ -29,6 +29,7 @@ program
 	.command('gui [environment]', 'gui for GraphiQL and Liquid Evaluator')
 	.command('logs [environment]', 'stream debugging logs from your website')
 	.command('init', 'create default folder structure for Siteglide Admin')
+	.command('check [path]', 'check Liquid code quality with platformos-check linter')
 	.command('deploy [environment]', 'upload all code to your site')
 	.command('export [environment]', 'export the code, assets and data from your site')
 	.command('migrate [environment] --url [url]', 'Static site migration into siteglide')
