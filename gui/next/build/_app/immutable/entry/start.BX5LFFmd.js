bfcc8c5b83285fc757bad6311c659578c6817af0
mattwalter91 <mattwalter91@gmail.com>
2026-09-04 09:37:57 +0100

    1.11.1 - Fix logo in light mode

--- gui/next/build/_app/immutable/entry/start.BX5LFFmd.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Cr7H4JEN.js";export{o as load_css,r as start};
---
+++ gui/next/build/_app/immutable/entry/start.CkzdVSKB.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/ByI04ZCe.js";export{o as load_css,r as start};
